5244338d37ad9c5dcba6a2b29ba61754967fc552
giulio_franco <giulio_franco@msn.com>
2012-11-11 18:43:47 +0000

    [MT] Aggiunti classe e diagramma di prova

---
+++ Impl/src/atsilo/entity/GenitoreDiProva.java
@@ -0,0 +1,23 @@
+package atsilo.entity;
+
+public final class GenitoreDiProva {
+    private int nome;
+    private int cognome;
+
+
+    public void setNome(int nome) {
+        this.nome = nome;
+    }
+
+    public int getNome() {
+        return nome;
+    }
+
+    public void setCognome(int cognome) {
+        this.cognome = cognome;
+    }
+
+    public int getCognome() {
+        return cognome;
+    }
+}
