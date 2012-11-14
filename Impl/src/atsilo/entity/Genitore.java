246cfe6e35b780b12c62c8fcb16b0e5caf4b2bc6
ferdi91d <ferdi91d@gmail.com>
2012-11-14 10:25:51 +0000

    Modifiche merge beans

--- Impl/src/atsilo/entity/Genitore.java
+++ Impl/src/atsilo/entity/Genitore.java
@@ -2,52 +2,15 @@ package atsilo.entity;
 
 import java.util.List;
 
-public class Genitore {
-    /**
-     * @attribute
-     */
-    private Date dataNascita;
+public class Genitore extends Utente {
 
-    /**
-     * @attribute
-     */
-    private String CodiceFiscale;
-
-    /**
-     * @attribute
-     */
-    private String ComuneNascita;
-
-    /**
-     * @attribute
-     */
-    private String Email;
-
-    /**
-     * @attribute
-     */
-    private String indirizzoResidenza;
 
     /**
      * @attribute
      */
     private String tipo;
 
-    /**
-     * @attribute
-     */
-    private String Cognome;
 
-    /**
-     * @attribute
-     */
-    private String Telefono;
-
-    /**
-     * @attribute
-     */
-    private String nome;
-    
     private  List<Bambino> figli;
     
     private List<Questionario> questionariCompilati;
@@ -55,59 +18,28 @@ public class Genitore {
     public Genitore() {
     }
 
-    public void setIndirizzoResidenza(String add) {
-    }
 
-    public void setTelefono(String t) {
-    }
-
-    public String getTelefono() {
-    }
-
-    public void setComuneNascita(String cm) {
-    }
-
-    public String getEmail() {
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
     }
 
     public String getTipo() {
+        return tipo;
     }
 
-    public String getComuneNascita() {
-    }
-
-    public String getCognome() {
-    }
-
-    public String getIndirizzoResidenza() {
-    }
-
-    public String getCodiceFiscale() {
-    }
-
-    public void setEmail(String m) {
-    }
-
-    public String getNome() {
-    }
-
-    public Date getDataNascita() {
-    }
-
-    public void setCodiceFiscale(Object cf) {
-    }
-
-    public void addFiglio(Bambino bambino) {
+    public void setFigli(List<Bambino> figli) {
+        this.figli = figli;
     }
 
     public List<Bambino> getFigli() {
-        return null;
+        return figli;
     }
 
-    public void addQuestionarioCompilato(Questionario questionario) {
+    public void setQuestionariCompilati(List<Questionario> questionariCompilati) {
+        this.questionariCompilati = questionariCompilati;
     }
 
     public List<Questionario> getQuestionariCompilati() {
-        return null;
+        return questionariCompilati;
     }
 }
