78344eda06414df6ebd7042405a51ece4c85c403
alfonso.piscitelli91@gmail.com <alfonso.piscitelli91@gmail.com@02549b1f-f713-6533-8de7-45dd18cc345b>
2012-11-14 11:00:37 +0000

    [WR] revision beans, added association

--- trunk/Impl/src/atsilo/entity/Psicopedagogo.java
+++ trunk/Impl/src/atsilo/entity/Psicopedagogo.java
@@ -1,7 +1,20 @@
 package atsilo.entity;
 
+<<<<<<< .mine
+public class Psicopedagogo extends Utente{
+    /**
+     * @attribute
+     */
+       private String numero_classi;
+=======
 public class Psicopedagogo extends Utente {
+>>>>>>> .r1309
 
+<<<<<<< .mine
+    /**
+     * @attribute
+     */
+=======
 
     /**
      * @attribute
@@ -11,16 +24,41 @@ public class Psicopedagogo extends Utente {
     /**
      * @attribute
      */
+>>>>>>> .r1309
     private String titoloDiStudi;
 
+<<<<<<< .mine
+   
+=======
 
+>>>>>>> .r1309
     public Psicopedagogo() {
     }
 
+<<<<<<< .mine
+=======
     public void setNumeroClassi(String numeroClassi) {
         this.numeroClassi = numeroClassi;
     }
+>>>>>>> .r1309
 
+<<<<<<< .mine
+    public void setNumero_classi(String numero_classi) {
+        this.numero_classi = numero_classi;
+    }
+
+    public String getNumero_classi() {
+        return numero_classi;
+    }
+
+    public void setTitolo_di_studio(String titolo_di_studio) {
+        this.titolo_di_studio = titolo_di_studio;
+    }
+
+    public String getTitolo_di_studio() {
+        return titolo_di_studio;
+    }
+=======
     public String getNumeroClassi() {
         return numeroClassi;
     }
@@ -32,4 +70,5 @@ public class Psicopedagogo extends Utente {
     public String getTitoloDiStudi() {
         return titoloDiStudi;
     }
+>>>>>>> .r1309
 }
