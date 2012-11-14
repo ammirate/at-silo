d0a3cf9b043fd8238b80dadcde9cc7997265c09c
ferdi91d <ferdi91d@gmail.com>
2012-11-14 10:38:24 +0000

    Modifiche merge beans

--- Impl/src/atsilo/entity/PersonaleAsilo.java
+++ Impl/src/atsilo/entity/PersonaleAsilo.java
@@ -1,44 +1,17 @@
 package atsilo.entity;
 
-public class PersonaleAsilo {
-    
-    private String nome;
-    private String cognome;
-    private String codFiscale;
+public class PersonaleAsilo extends Utente {
+
     private String categoriaAppartenza;
     
     public PersonaleAsilo() {
     }
 
-    public String getNome() {
-            return nome;
-    }
-
-    public void setNome(String nome) {
-            this.nome = nome;
-    }
-
-    public String getCognome() {
-            return cognome;
-    }
-
-    public void setCognome(String cognome) {
-            this.cognome = cognome;
-    }
-
-    public String getCodFiscale() {
-            return codFiscale;
-    }
-
-    public void setCodFiscale(String codFiscale) {
-            this.codFiscale = codFiscale;
+    public void setCategoriaAppartenza(String categoriaAppartenza) {
+        this.categoriaAppartenza = categoriaAppartenza;
     }
 
     public String getCategoriaAppartenza() {
-            return categoriaAppartenza;
-    }
-
-    public void setCategoriaAppartenza(String categoriaAppartenza) {
-            this.categoriaAppartenza = categoriaAppartenza;
+        return categoriaAppartenza;
     }
 }
--- Impl/src/atsilo/entity/ResponsabileQuestionario.java
+++ Impl/src/atsilo/entity/ResponsabileQuestionario.java
@@ -1,41 +1,9 @@
 package atsilo.entity;
 
-public class ResponsabileQuestionario {
-    /**
-     * @attribute
-     */
-    private String cognome;
+public class ResponsabileQuestionario extends Utente {
 
-    /**
-     * @attribute
-     */
-    private String codiceFiscale;
-
-    /**
-     * @attribute
-     */
-    private String email;
-
-    /**
-     * @attribute
-     */
-    private String nome;
 
     public ResponsabileQuestionario() {
     }
 
-    public void setEmail(String m) {
-    }
-
-    public String getNome() {
-    }
-
-    public String getCognome() {
-    }
-
-    public String getCodiceFiscale() {
-    }
-
-    public Sting getEmail() {
-    }
 }
