2c8605cb9b2523b3d512c5a4054b1219137b0f87
ano2607 <cesarano2607@gmail.com>
2012-11-18 18:38:40 +0000

    [WR_T] Completata parte team3 (questionario e registro)

---
+++ Impl/src/application/ControlQuestionario.java
@@ -0,0 +1,27 @@
+package application;
+
+import atsilo.entity.*;
+import java.sql.Date;
+
+import java.util.List;
+
+public class ControlQuestionario {
+    
+    public ControlQuestionario() {
+    }
+    
+    public void inserisciQuestionario(Questionario questionario){}
+    
+    public Questionario eliminaQuestionario(String id){
+        return null;
+    }
+    
+    public void modificaQuestionario(List<DomandaQuestionario> domande){}
+    
+    public void spostaDataInizio(Date newData){}
+    
+    public void spostaDataFine(Date newData){}
+    
+ 
+    
+}
--- Impl/src/application/ControlRegistro.java
+++ Impl/src/application/ControlRegistro.java
@@ -12,50 +12,9 @@ public class ControlRegistro {
         
     }
     
-    //---------- Assenze---------
-    public void inserisciAssenza(Bambino bambino, Date data){}
+    public void inserisciRegistro(Registro registro){}
     
-    public Assenza eliminaAssenza(Bambino bambino, Date data){
-        return null;
-    }
-            
-    public List<Assenza> visualizzaAssenzeGiorno(Date data){
-        return null;  
-    }
-    //-----------------------------
-    
- 
-    public void inserisciAttivita(Attivita attivita){}
-    
-    public void modificaAttivita(String attivita){}
-    
-    public Attivita eliminaAttivita(String titolo){
-        return null;
-    }
-    
-    public Attivita ricercaAttivitaPerTitolo(String titolo){
-        return null;
-    }
-    
-    public List<Attivita> ricercaAttivitaPerCategoria(String categoria){
-        return null;
-    }
-    
-    public void inserisciEvento(Evento evento){}
-    
-    public void modificaEvento(Evento evento){}
-    
-    public Evento eliminaEvento(Evento evento){
-        return null;
-    }
-
-    public void spostaEvento(Evento evento, Date newData){}
-    
-    public List<Evento> getEventiPerPersonale(PersonaleAsilo pers){
-        return null;
-    }
-    
-    public List<Evento> getEventiInData(Date data){
+    public Registro getRegistro(Classe classe){
         return null;
     }
     
