7acab7dfafee659e6e74d2d251bd77c28fbbf74f
mariellaferrara1 <mariellaferrara1@gmail.com>
2012-11-19 17:41:30 +0000

    [WD M] Application Class (M)

---
+++ Impl/src/application/ControlMenu.java
@@ -0,0 +1,50 @@
+package application;
+
+import atsilo.entity.MenuMensa;
+import java.sql.Date;
+
+import java.util.List;
+
+
+public class ControlMenu {
+
+    public ControlMenu{
+        
+    }
+     public void inserisciMenuMensa (String id, String categoriaBambino, String tipologia, String primo, String secondo, String contorno, String frutta, Date data){
+                          
+                      }
+          
+     public void modificaMenuMensa (MenuMensa m, MenuMensa newM){
+         
+     }
+     public MenuMensa eliminaMenù (MenuMensa m){
+         
+     }
+     public List<MenuMensa> VisualizzaMenu(){
+         
+     }
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
