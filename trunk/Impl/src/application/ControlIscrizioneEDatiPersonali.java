fc8b1b4e5c72c6897a7ca164a52546c5135e6d56
gianfranco.b.d@gmail.com <gianfranco.b.d@gmail.com@02549b1f-f713-6533-8de7-45dd18cc345b>
2012-11-19 21:51:32 +0000

    [WT A] modidica control Team 1

---
+++ trunk/Impl/src/application/ControlIscrizioneEDatiPersonali.java
@@ -0,0 +1,31 @@
+package application;
+
+public class ControlIscrizioneEDatiPersonali {
+    ControlIscrizioneEDatiPersonali() {
+    }
+
+    String getValoreStatoIscrizione(Bambino bambino) {
+        return "";
+    }
+
+    Genitore getDatiGenitore(Genitore genitore) {
+        return null;
+    }
+
+    Bambino getDatiBambino(Bambino bambino) {
+        return null;
+    }
+
+    void inserisciGenitore(Genitore genitore) {
+    }
+
+    DomandaIscrizione getDomanda() {
+        return null;
+    }
+
+    void inserisciIscritto(DomandaIscrizione iscrizione) {
+    }
+
+    void inserisciDomandaIscirizione(DomandaIscrizione iscrizione) {
+    }
+}
