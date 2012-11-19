19f1b9c070e4118aef5f0f50db64d1eccf78395d
mariellaferrara1@gmail.com <mariellaferrara1@gmail.com@02549b1f-f713-6533-8de7-45dd18cc345b>
2012-11-19 17:41:30 +0000

    [WD M] Application Class (M)

---
+++ trunk/Impl/src/application/ControlServizi.java
@@ -0,0 +1,44 @@
+package application;
+
+import atsilo.entity.Bambino;
+import atsilo.entity.OrarioUtente;
+import atsilo.entity.Servizio;
+
+public class ControlServizi {
+
+public ControlServizi(){
+    
+}
+
+public inserisciServizio (String orainizio, String orafino, String pianopasto, Bambino b, OrarioUtente o, String id){
+    
+}
+public void modificaServizio(Servizio s, Servizio newS){
+    
+}
+public void modificaPianoPasto(String idServ, String pianopasto){
+    
+}
+public eliminaPianoPasto(String idServ){
+    
+}
+public String visualizzaPianoPasto(String idServ){
+    
+}
+public void 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ trunk/Impl/src/application/ControlTirocinanti.java
@@ -0,0 +1,47 @@
+package application;
+
+import atsilo.entity.Feedback;
+import atsilo.entity.PersonaleAsilo;
+import atsilo.entity.Tirocinante;
+
+import java.sql.Date;
+
+import java.util.List;
+
+public class ControlTirocinanti {
+
+    public ControlTirocinanti(){
+        
+    }
+
+
+    public void inserisciTirocinante(Date ddn, String nome, String cognome, String cF, String email, String cdn, 
+                                     String tel, String res, PersonaleAsilo p, int oreT, int oreL){
+        
+    }
+    public void modificaTirocinante(Tirocinante t, Tirocinante newT){
+        
+    }
+    public Tirocinante eliminaTirocinante(Tirocinante t){
+        
+    }
+    public void inserisciAttivitaTirocinante(){
+        
+    }
+    public void modificaAttivitaTirocinante(){
+        
+    }
+    public AttivitaTirocinante eliminaAttivitaTirocinante(){
+        
+    }
+    public List<Tirocinante> visualizzaTirocinanti(){
+        
+    }
+    public void inserisciFeedback (int id, String descrizione, PersonaleAsilo p){
+        
+    }
+    public Feedback eliminaFeedback(Feedback f){
+        
+    }
+
+}
