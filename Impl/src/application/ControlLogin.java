46d1c77ceb4137c2ce2faf96368211b6e3c2c27b
gianfranco.b.d <gianfranco.b.d@gmail.com>
2012-11-19 22:11:30 +0000

    [WT A] modidica control Team 1

---
+++ Impl/src/application/ControlLogin.java
@@ -0,0 +1,12 @@
+package application;
+
+import atsilo.entity.Utente;
+
+public class ControlLogin {
+    ControlLogin() {
+    }
+
+    Boolean getValoreLogin(Utente utente) {
+        return Boolean.TRUE;
+    }
+}
---
+++ Impl/src/application/ControlRicercaDati.java
@@ -0,0 +1,10 @@
+package application;
+
+public class ControlRicercaDati {
+    ControlRicercaDati() {
+    }
+
+    List<Utente> getUtenteRicercato() {
+        return null;
+    }
+}
