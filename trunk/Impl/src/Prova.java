82cb102b561791fb0149e886d7ddea4ebd5cbdec
funforums@gmail.com <funforums@gmail.com@02549b1f-f713-6533-8de7-45dd18cc345b>
2012-11-20 18:51:45 +0000

    [MT] Commit di prova da eclipse con SVNKit 1.7

---
+++ trunk/Impl/src/Prova.java
@@ -0,0 +1,24 @@
+/**
+ * Progetto \@silo
+ * Copyleft
+ */
+
+/**
+ * Classe Prova
+ * <Descrizione classe>
+ * 
+ * @author Alfonso Murolo <funforums@gmail.com>
+ * 
+ */
+public class Prova {
+    //Logger
+    //private static final java.util.Logger LOG = ...
+    
+    //Variabili di istanza
+    
+    
+    //Costruttori
+    
+    
+    //Metodi
+}
