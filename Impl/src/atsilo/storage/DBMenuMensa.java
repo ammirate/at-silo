5d3f7db222b20181893dcdeecc09f14b86f38cfa
marco.parisi90 <marco.parisi90@gmail.com>
2012-11-20 15:50:16 +0000

    [WT M] Aggiunta di storage  (feedback, programmaEducativoSettimanale, menuMensa)

--- Impl/src/atsilo/storage/DBMenuMensa.java
+++ Impl/src/atsilo/storage/DBMenuMensa.java
@@ -1,6 +1,13 @@
 package atsilo.storage;
 
+import atsilo.entity.Feedback;
 import atsilo.entity.MenuMensa;
+
+import atsilo.entity.Tirocinante;
+
+import java.sql.Date;
+
+import java.util.List;
 
 public class DBMenuMensa extends DBBeans {
     Tabella tabella;
@@ -8,4 +15,42 @@ public class DBMenuMensa extends DBBeans {
     public DBMenuMensa(){
         tabella= new Tabella("MenuMensa");
         super(tabella);
-    }}
+    }
+    
+    public List<MenuMensa> ricercaMenuMensaPerCategoriaBambino(String categoria){
+        List<MenuMensa> menu;
+        return menu;   
+    }
+    public List<MenuMensa> ricercaMenuMensaPerTipologia(String Tipologia){
+        List<MenuMensa> menu;
+        return menu;   
+        }
+    
+    public List<MenuMensa> ricercaMenuMensaPerPrimoPasto(String primoPasto){
+        List<MenuMensa> menu;
+        return menu;   
+        }
+    
+    public List<MenuMensa> ricercaMenuMensaPerSecondoPasto(String secondoPasto){
+        List<MenuMensa> menu;
+        return menu;   
+        }
+    
+    public List<MenuMensa> ricercaMenuMensaPerContorno(String contorno){
+        List<MenuMensa> menu;
+        return menu;   
+        }
+    
+     public List<MenuMensa> ricercaMenuMensaPerFrutta(String frutta){
+        List<MenuMensa> menu;
+        return menu;   
+        }
+     
+    public List<MenuMensa> ricercaMenuMensaPerData(Date data){
+         List<MenuMensa> menu;
+        return menu;   
+        }
+                                            
+    
+    
+    }
