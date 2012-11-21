921a7f891ab3cd44d61c7b84fb807a5c648417e1
lomastoluigi <lomastoluigi@gmail.com>
2012-11-21 11:55:06 +0000

    [wt] rimodificati i metodi di alcune classi dopo la modifica dei costruttori

--- Impl/src/atsilo/storage/DBAssenza.java
+++ Impl/src/atsilo/storage/DBAssenza.java
@@ -1,10 +1,12 @@
 package atsilo.storage;
-
+import java.util.Date;
 import atsilo.entity.Assenza;
 
 public class DBAssenza extends DBBeans {
+    Tabella tabella;
     
-    public DBAssenza(Database db){
-        super("Assenza",db);
-    }
+    public DBAssenza(Database db){super("Assenza",db);}
+
+
+    public List<Assenza> ricercaAssenzaPerData(Date d){}
 }
