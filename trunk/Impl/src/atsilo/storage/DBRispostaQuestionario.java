2d046f79d99297fe9502607b8fc3e91a142737ef
ferdi91d@gmail.com <ferdi91d@gmail.com@02549b1f-f713-6533-8de7-45dd18cc345b>
2012-11-27 19:35:05 +0000

    [WT_A] prime classi storage (risposta questionario da completare)

--- trunk/Impl/src/atsilo/storage/DBRispostaQuestionario.java
+++ trunk/Impl/src/atsilo/storage/DBRispostaQuestionario.java
@@ -6,21 +6,76 @@ import atsilo.entity.Psicopedagogo;
 import atsilo.entity.Questionario;
 import atsilo.entity.RispostaQuestionario;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 
 public class DBRispostaQuestionario extends DBBeans {
     
+    private static final Map<String,String> MAPPINGS=creaMapping();
+    private static final List<String> CHIAVE=creaChiave(); 
+    
     public DBRispostaQuestionario(Database db){ super("RispostaQuestionario",db);}
     
+        /**
+     * @return
+     */
+    private static List<String> creaChiave() {
+        // TODO Scheletro generato automaticamente
+        return null;
+    }
+
+        /**
+     * @return
+     */
+    private static Map<String, String> creaMapping() {
+        // TODO Scheletro generato automaticamente
+        return null;
+    }
+
         public RispostaQuestionario ricercaRispostaQuestionarioPerId(String Id){
-            RispostaQuestionario c;
+            RispostaQuestionario c = null;
             return c;
             }
         
     public RispostaQuestionario ricercaRispostaQuestionarioPerValore(String valore){
-        RispostaQuestionario c;
+        RispostaQuestionario c = null;
         return c;
         }
+
+    /**
+     * @see atsilo.storage.DBBeans#getMappingFields()
+     */
+    @Override
+    protected Map getMappingFields() {
+        return MAPPINGS;
+    }
+
+    /**
+     * @see atsilo.storage.DBBeans#getKeyFields()
+     */
+    @Override
+    protected List getKeyFields() {
+        return CHIAVE;
+    }
+
+    /**
+     * @see atsilo.storage.DBBeans#creaBean(java.sql.ResultSet)
+     */
+    @Override
+    protected RispostaQuestionario creaBean(ResultSet r) throws SQLException {
+        RispostaQuestionario ris = null;
+        if(r.next())
+        {
+           ris.setId(r.getString("id"));
+           ris.setValore(r.getString("valore"));
+           ris.setGenitore((Genitore) r.getObject("genitore"));
+           ris.setQuestionario((Questionario) r.getObject("questionario"));
+           
+    }
+        return ris;
+}
 }
 
     
--- trunk/Impl/src/atsilo/storage/DBServizio.java
+++ trunk/Impl/src/atsilo/storage/DBServizio.java
@@ -1,17 +1,96 @@
 package atsilo.storage;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import atsilo.entity.Bambino;
+import atsilo.entity.OrarioUtente;
 import atsilo.entity.Servizio;
 
 public class DBServizio extends DBBeans {
+    
+    private static final Map<String,String> MAPPINGS=creaMapping();
+    private static final List<String> CHIAVE=creaChiave(); 
     
     public DBServizio(Database db){
         super("Servizio",db);
     }
 
-    public Servizio ricercaServizioPerBambino(String cf) {
-        return null;
+    /**
+     * @return
+     */
+    private static Map<String, String> creaMapping() {
+        Map<String,String> res= new HashMap<String,String>();
+        res.put("id", "id");
+        res.put("orario_fine","orarioFine");
+        res.put("orario_inizio","orarioInizio");
+        res.put("piano_pasto","pianoPasto");
+        res.put("bambino","bambino");
+        res.put("orario_utente", "orarioUtente");
+        return Collections.unmodifiableMap(res);
     }
 
+    /**
+     * @return
+     */
+    private static List<String> creaChiave() {
+        List<String> res=  Arrays.asList("id"); 
+        return Collections.unmodifiableList(res);
+    }
 
+    public Servizio ricercaServizioPerBambino(Bambino b) throws SQLException {
+        Servizio s= null;
+        ResultSet r = tabella.getDatabase().directQuery("SELECT * FROM " + tabella.getNomeTabella() + "WHERE bambino =" + b);
+        if(r.next()){
+            s.setId(r.getString("id"));
+            s.setOrarioFine(r.getString("orario_fine"));
+            s.setOrarioInizio(r.getString("orario_inizio"));
+            s.setPianoPasto(r.getString("piano_pasto"));
+            s.setBambino((Bambino) r.getObject("bambino"));
+            s.setOrarioUtente((OrarioUtente) r.getObject("orario_utente"));
+        }
+        r.close();
+        return s;
+    }
+
+    /**
+     * @see atsilo.storage.DBBeans#getMappingFields()
+     */
+    @Override
+    protected Map getMappingFields() {
+        return MAPPINGS;
+    }
+
+    /**
+     * @see atsilo.storage.DBBeans#getKeyFields()
+     */
+    @Override
+    protected List getKeyFields() {
+        return CHIAVE;
+    }
+
+    /**
+     * @see atsilo.storage.DBBeans#creaBean(java.sql.ResultSet)
+     */
+    @Override
+    protected Servizio creaBean(ResultSet r) throws SQLException {
+        Servizio s =null;
+        if(r.next())
+        {
+            s.setId(r.getString("id"));
+            s.setOrarioFine(r.getString("orario_fine"));
+            s.setOrarioInizio(r.getString("orario_inizio"));
+            s.setPianoPasto(r.getString("piano_pasto"));
+            s.setBambino((Bambino) r.getObject("bambino"));
+            s.setOrarioUtente((OrarioUtente) r.getObject("orario_utente"));
+    }
+        return s;
+
+
+    }
 }
