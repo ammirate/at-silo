d93aee8fcdb8fdc5ff1e3286d87b122cb2fa98dc
ano2607 <cesarano2607@gmail.com>
2012-11-27 16:10:06 +0000

    package contenente eccezioni

---
+++ Impl/src/atsilo/exception/RegistroException.java
@@ -0,0 +1,40 @@
+/*
+ *-----------------------------------------------------------------
+ * This file is licensed under GPL 3.0:
+ * http://www.gnu.org/licenses/gpl-3.0.html
+ *-----------------------------------------------------------------
+ * FILE: RegistroException.java
+ *-----------------------------------------------------------------
+ * PROGETTO: Atsilo
+ *-----------------------------------------------------------------
+ * OWNER
+ * Antonio, 27/nov/2012
+ * REVISION
+ * <nome revisore>, <data revisione>
+ *-----------------------------------------------------------------
+ */
+
+package atsilo.exception;
+
+/**
+ * Classe RegistroException
+ * <Descrizione classe>
+ * 
+ * @author Antonio
+ * 
+ */
+public class RegistroException {
+    //Logger
+    /*
+     * private static final java.util.Logger LOG
+     *         = Logger.getLogger(RegistroException.class.getName());
+     */
+    
+    //Variabili di istanza
+    
+    
+    //Costruttori
+    
+    
+    //Metodi
+}
