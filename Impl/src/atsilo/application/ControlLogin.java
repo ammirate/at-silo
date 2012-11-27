8b40c60503274710c21fd2b51c5c245fbbaa80d1
marco.parisi90 <marco.parisi90@gmail.com>
2012-11-27 15:40:34 +0000

    [Wr M] Bozza controllogin

--- Impl/src/atsilo/application/ControlLogin.java
+++ Impl/src/atsilo/application/ControlLogin.java
@@ -1,14 +1,32 @@
 package atsilo.application;
 
 import atsilo.entity.Utente;
+import atsilo.storage.DBAccount;
+import atsilo.storage.DBEducatoreDidattico;
+import atsilo.storage.DBEventPlanner;
+import atsilo.storage.DBGenitore;
+import atsilo.storage.DBPersonaleAsilo;
+import atsilo.storage.DBPsicopedagogo;
+import atsilo.storage.DBResponsabileQuestionario;
+import atsilo.storage.DBTirocinante;
+import atsilo.storage.Database;
 
 public class ControlLogin {
     private static final ControlLogin INSTANCE;
-
+    private DBAccount dbAccount;
+    private DBGenitore dbGenitore;
+    private DBEducatoreDidattico dbEducatoreDidattico;
+    private DBEventPlanner dbEventPlanner;
+    private DBPersonaleAsilo dbPersonaleAsilo;
+    private DBPsicopedagogo  dbPsicopedagogo;
+    private DBResponsabileQuestionario  dbResponsabileQuestionario;
+    private DBTirocinante dbTirocinante;
+    
     ControlLogin() {
         //Come prima cosa, bisogna creare un'istanza di database e aprire una connessione
         Database db = new Database();
-        if (!db.apriConnessione()) {
+        if (!db.apriConnessione()) 
+        {
             /*
              * Connessione fallita
              * Bisogna restituire un errore all'utente
@@ -17,15 +35,15 @@ public class ControlLogin {
 
         //Quindi, si possono creare tutti i gestori di tabelle necessari
         try {
-            DBAccount dbAccount = new DBAccount(db);
-            DBGenitore dbGenitore = new DBGenitore(db);
-
-            /*
-             * Qui si possono fare tutte le operazioni necessarie
-             * sui gestori di tabelle.
-             */
-            return xxx;
-
+            dbAccount = new DBAccount(db);
+            dbGenitore = new DBGenitore(db);
+            dbEducatoreDidattico = new DBEducatoreDidattico(db);
+            dbEventPlanner = new DBEventPlanner(db);
+            dbPersonaleAsilo = new DBPersonaleAsilo(db);
+            dbPsicopedagogo = new DBPsicopedagogo(db);
+            dbResponsabileQuestionario = new DBResponsabileQuestionario(db);
+            dbTirocinante = new DBTirocinante(db);
+            
         } finally {
             /*
              * Alla fine dell'interazione, prima di uscire dal metodo,
