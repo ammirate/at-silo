98ed51231bc50a092d819010ff97918763f97acb
marco.parisi90@gmail.com <marco.parisi90@gmail.com@02549b1f-f713-6533-8de7-45dd18cc345b>
2012-11-27 15:00:20 +0000

    [WR M] bozza controlLogin

--- trunk/Impl/src/atsilo/application/ControlLogin.java
+++ trunk/Impl/src/atsilo/application/ControlLogin.java
@@ -6,9 +6,38 @@ public class ControlLogin {
     private static final ControlLogin INSTANCE;
 
     ControlLogin() {
+        //Come prima cosa, bisogna creare un'istanza di database e aprire una connessione
+        Database db = new Database();
+        if (!db.apriConnessione()) {
+            /*
+             * Connessione fallita
+             * Bisogna restituire un errore all'utente
+             */
+        }
+
+        //Quindi, si possono creare tutti i gestori di tabelle necessari
+        try {
+            DBAccount dbAccount = new DBAccount(db);
+            DBGenitore dbGenitore = new DBGenitore(db);
+
+            /*
+             * Qui si possono fare tutte le operazioni necessarie
+             * sui gestori di tabelle.
+             */
+            return xxx;
+
+        } finally {
+            /*
+             * Alla fine dell'interazione, prima di uscire dal metodo,
+             * bisogna chiudere la connessione.
+             */
+            db.chiudiConnessione();
+        }
     }
 
-    Boolean getValoreLogin(Utente utente) {
+    Boolean getValoreLogin(String username, String password) {
+        
+        
         return false;
     }
 
