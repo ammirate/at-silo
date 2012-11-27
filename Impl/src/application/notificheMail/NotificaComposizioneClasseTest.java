df83702007e1096d844711b66bd22176ae77cca6
gianfranco.b.d <gianfranco.b.d@gmail.com>
2012-11-27 18:35:48 +0000

    [WT A] modifica di gestione bando e di notifiche mail

--- Impl/src/application/notificheMail/NotificaComposizioneClasseTest.java
+++ Impl/src/application/notificheMail/NotificaComposizioneClasseTest.java
@@ -1,7 +1,18 @@
 /**
- * Progetto \@silo
- * Copyleft
- */
+ *-----------------------------------------------------------------
+ * This file is licensed under GPL 3.0:
+ * http://www.gnu.org/licenses/gpl-3.0.html
+ *-----------------------------------------------------------------
+ * FILE: NotificaComposizioneClasseTest.java
+ *-----------------------------------------------------------------
+ * PROGETTO: Atsilo
+ *-----------------------------------------------------------------
+ * OWNER
+ * Bottiglieri Gianfranco, 25/11/2012 
+ * REVISION
+ * nome revisore, data revisione
+ *-----------------------------------------------------------------
+ **/
 package application.notificheMail;
 
 import static org.junit.Assert.*;
