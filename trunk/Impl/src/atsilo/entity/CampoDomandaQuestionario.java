ff3868d7000907fe31b72cd602cca4624196cbec
lomastoluigi@gmail.com <lomastoluigi@gmail.com@02549b1f-f713-6533-8de7-45dd18cc345b>
2012-11-29 14:31:12 +0000

    [WT] implementati i metodi get e set

--- trunk/Impl/src/atsilo/entity/CampoDomandaQuestionario.java
+++ trunk/Impl/src/atsilo/entity/CampoDomandaQuestionario.java
@@ -10,4 +10,76 @@ public class CampoDomandaQuestionario {
     public CampoDomandaQuestionario() {
         super();
     }
+
+    /**
+     * @return tipo
+     */
+    public String getTipo() {
+        return tipo;
+    }
+
+    /**
+     * @param tipo nuovo tipo
+     */
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    /**
+     * @return descrizione
+     */
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    /**
+     * @param descrizione nuovo descrizione
+     */
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    /**
+     * @return valore
+     */
+    public String getValore() {
+        return valore;
+    }
+
+    /**
+     * @param valore nuovo valore
+     */
+    public void setValore(String valore) {
+        this.valore = valore;
+    }
+
+    /**
+     * @return domanda
+     */
+    public DomandaQuestionario getDomanda() {
+        return domanda;
+    }
+
+    /**
+     * @param domanda nuovo domanda
+     */
+    public void setDomanda(DomandaQuestionario domanda) {
+        this.domanda = domanda;
+    }
+
+    /**
+     * @return risposta
+     */
+    public RispostaQuestionario getRisposta() {
+        return risposta;
+    }
+
+    /**
+     * @param risposta nuovo risposta
+     */
+    public void setRisposta(RispostaQuestionario risposta) {
+        this.risposta = risposta;
+    }
+
+  
 }
