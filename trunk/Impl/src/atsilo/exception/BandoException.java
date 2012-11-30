3ce18b14adeb3d1345b8f4463b67018fabe8c0d8
gianfranco.b.d@gmail.com <gianfranco.b.d@gmail.com@02549b1f-f713-6533-8de7-45dd18cc345b>
2012-11-30 08:40:08 +0000

    [WT A] aggiunta di bandoException

---
+++ trunk/Impl/src/atsilo/exception/BandoException.java
@@ -0,0 +1,25 @@
+/*
+ *-----------------------------------------------------------------
+ * This file is licensed under GPL 3.0:
+ * http://www.gnu.org/licenses/gpl-3.0.html
+ *-----------------------------------------------------------------
+ * FILE: DBConnectionException.java
+ *-----------------------------------------------------------------
+ * PROGETTO: Atsilo
+ *-----------------------------------------------------------------
+ * OWNER
+ * Gianfranco, 27/nov/2012
+ * REVISION
+ * <nome revisore>, <data revisione>
+ *-----------------------------------------------------------------
+ */
+
+package atsilo.exception;
+
+
+public class BandoException extends Exception{
+
+    public BandoException(String s){
+        super(s);
+    }
+}
