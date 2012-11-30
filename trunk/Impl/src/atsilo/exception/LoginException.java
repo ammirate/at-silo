542b8853c3514ef51947d34c8bd3b8a90f14fe4f
gianfranco.b.d@gmail.com <gianfranco.b.d@gmail.com@02549b1f-f713-6533-8de7-45dd18cc345b>
2012-11-30 08:35:09 +0000

    [WT a] aggiunta di login exception

---
+++ trunk/Impl/src/atsilo/exception/LoginException.java
@@ -0,0 +1,25 @@
+/*
+ *-----------------------------------------------------------------
+ * This file is licensed under GPL 3.0:
+ * http://www.gnu.org/licenses/gpl-3.0.html
+ *-----------------------------------------------------------------
+ * FILE: DBConnectionException.java
+ *-----------------------------------------------------------------
+ * PROGETTO: Atsilo
+ *-----------------------------------------------------------------
+ * OWNER
+ * Gianfranco Bottiglieri, 30/nov/2012
+ * REVISION
+ * <nome revisore>, <data revisione>
+ *-----------------------------------------------------------------
+ */
+
+package atsilo.exception;
+
+
+public class LoginException extends Exception{
+
+    public LoginException(String s){
+        super(s);
+    }
+}
