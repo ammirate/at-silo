e709a041b16453875251a8a7a073adaddb9f87a4
ano2607 <cesarano2607@gmail.com>
2012-12-04 20:47:14 +0000

    [WT] test registro

---
+++ Impl/src/atsilo/test/application/DriverControlRegistro.java
@@ -0,0 +1,48 @@
+/*
+ *-----------------------------------------------------------------
+ * This file is licensed under GPL 3.0:
+ * http://www.gnu.org/licenses/gpl-3.0.html
+ *-----------------------------------------------------------------
+ * FILE: DriverControlRegistro.java
+ *-----------------------------------------------------------------
+ * PROGETTO: Atsilo
+ *-----------------------------------------------------------------
+ * OWNER
+ * Antonio, 04/dic/2012
+ * REVISION
+ * <nome revisore>, <data revisione>
+ *-----------------------------------------------------------------
+ */
+
+package atsilo.test.application;
+
+import atsilo.entity.Classe;
+import atsilo.entity.Registro;
+import atsilo.exception.DBConnectionException;
+import atsilo.exception.RegistroException;
+
+/**
+ * Classe DriverControlRegistro
+ * <Descrizione classe>
+ * 
+ * @author Antonio
+ * 
+ */
+public class DriverControlRegistro {
+    
+    static ControlRegistroTest cr;
+
+    public static void main(String[] args) throws DBConnectionException, RegistroException{
+        
+        //test1
+        cr = ControlRegistroTest.getIstance();
+        Classe classe = new Classe("primaA",null);
+        
+        Registro r1 = cr.getRegistro(classe);
+        System.out.println("Prova #1: "+r1);
+    }
+    
+    
+    
+    
+}
