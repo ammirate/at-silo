c5fffe6dcddff5449ff0615505e4f107ee4ff428
gianfranco.b.d <gianfranco.b.d@gmail.com>
2012-12-04 11:58:12 +0000

    [WT A] cambiamento del metodo ricercautente in stubUtente

---
+++ Impl/src/atsilo/test/application/ControlGestioneBandoTest.java
@@ -0,0 +1,56 @@
+/*
+ *-----------------------------------------------------------------
+ * This file is licensed under GPL 3.0:
+ * http://www.gnu.org/licenses/gpl-3.0.html
+ *-----------------------------------------------------------------
+ * FILE: ControlGestioneBandoTest.java
+ *-----------------------------------------------------------------
+ * PROGETTO: Atsilo
+ *-----------------------------------------------------------------
+ * OWNER
+ * Gianfranco Bottiglieri, 30/nov/2012
+ * REVISION
+ * <nome revisore>, <data revisione>
+ *-----------------------------------------------------------------
+ */
+
+package atsilo.test.application;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ControlGestioneBandoTest {
+    
+    /**
+     * Test method for {@link atsilo.application.ControlGestioneBando#insrisciPunteggio(atsilo.entity.DomandaIscrizione, int)}.
+     */
+    @Test
+    public final void testInsrisciPunteggio() 
+    {
+        fail("Not yet implemented"); // TODO
+    }
+    
+    //Logger
+    /*
+     * private static final java.util.Logger LOG
+     *         = Logger.getLogger(ControlGestioneBandoTest.class.getName());
+     */
+    
+    /**
+     * Test method for {@link atsilo.application.ControlGestioneBando#inserisciIntervalloBando(java.lang.String, java.lang.String)}.
+     */
+    @Test
+    public final void testInserisciIntervalloBando() {
+        fail("Not yet implemented"); // TODO
+    }
+    
+    /**
+     * Test method for {@link atsilo.application.ControlGestioneBando#modificaBando(atsilo.entity.Bando, java.lang.String, java.lang.String)}.
+     */
+    @Test
+    public final void testModificaBando() {
+        fail("Not yet implemented"); // TODO
+    }
+    
+}
