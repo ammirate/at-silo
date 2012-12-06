5d355f1c5ce08c728912f45985ae0783a7dbb67a
gianfranco.b.d@gmail.com <gianfranco.b.d@gmail.com@02549b1f-f713-6533-8de7-45dd18cc345b>
2012-12-06 15:00:41 +0000

    [wr a] aggiunta di un metodo mancane senza implementazione

--- trunk/Impl/src/atsilo/storage/DBBando.java
+++ trunk/Impl/src/atsilo/storage/DBBando.java
@@ -31,52 +31,57 @@ import java.util.Collections;
 
 public class DBBando extends DBBeans<Bando> {
     
-    private static final Map<String,String> MAPPINGS=creaMapping();
-    private static final List<String> CHIAVE=creaChiave();
+    private static final Map<String, String> MAPPINGS = creaMapping();
+    private static final List<String> CHIAVE = creaChiave();
+    
     
     /**
      * 
      * @param db
      */
-    public DBBando(Database db){super("Bando",db);}
+    public DBBando(Database db) {
+        super("Bando", db);
+    }
     
     
     /**
-     * metodo che asocia alla chiave(nome campo db) il rispettivo valore(nome attributo classe)
+     * metodo che asocia alla chiave(nome campo db) il rispettivo valore(nome
+     * attributo classe)
+     * 
      * @return
      */
-    private static Map<String,String> creaMapping()
-    {
-        Map<String,String> res= new HashMap<String,String>();
-        res.put("id","id");// la classe Assenza del package entity ha come attributi (data e bambino)????
-        res.put("data_inizio","dataInizio");
-        res.put("data_fine","dataFine");
-       
+    private static Map<String, String> creaMapping() {
+        Map<String, String> res = new HashMap<String, String>();
+        res.put("id", "id");// la classe Assenza del package entity ha come
+                            // attributi (data e bambino)????
+        res.put("data_inizio", "dataInizio");
+        res.put("data_fine", "dataFine");
+        
         
         return Collections.unmodifiableMap(res);
     }
     
     /**
      * Metodo che crea la chiave
+     * 
      * @return
      */
-    private static List<String> creaChiave()
-    {
-        List<String> res=  Arrays.asList("id");
+    private static List<String> creaChiave() {
+        List<String> res = Arrays.asList("id");
         
         return Collections.unmodifiableList(res);
     }
     
     
-
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see atsilo.storage.DBBeans#creaBean(java.sql.ResultSet)
      */
     @Override
     protected Bando creaBean(ResultSet r) throws SQLException {
-        Bando b=new Bando();
-        if(r.next())
-        {
+        Bando b = new Bando();
+        if (r.next()) {
             b.setiD(r.getInt("id"));
             b.setDataInizio(r.getString("data_inizio"));
             b.setDataFine(r.getString("data_fine"));
@@ -85,98 +90,124 @@ public class DBBando extends DBBeans<Bando> {
         return b;
     }
     
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see atsilo.storage.DBBeans#getMappingFields()
      */
     @Override
-    public Map getMappingFields() {return MAPPINGS;}
-
-    /* (non-Javadoc)
+    public Map getMappingFields() {
+        return MAPPINGS;
+    }
+    
+    /*
+     * (non-Javadoc)
+     * 
      * @see atsilo.storage.DBBeans#getKeyFields()
      */
     @Override
-    public List getKeyFields() { return CHIAVE; }
+    public List getKeyFields() {
+        return CHIAVE;
+    }
     
     
     /**
      * cerca bando per il valore di un id specifico.
-     * @param id, identificativo usato per la ricerca del bando
-     * @return restituisce il bando avente id ricercato oppure un bando vuoto nel caso in cui non trova un id
-     * @throws SQLException nel caso in cui si verifica un errore di connessione 
+     * 
+     * @param id
+     *            , identificativo usato per la ricerca del bando
+     * @return restituisce il bando avente id ricercato oppure un bando vuoto
+     *         nel caso in cui non trova un id
+     * @throws SQLException
+     *             nel caso in cui si verifica un errore di connessione
      */
-    public Bando cercaBandoPerId (int id) throws SQLException 
-    {
-       Bando b=new Bando();
-       
-       PreparedStatement stmt = tabella.prepareStatement(
-               "SELECT * FROM " + tabella.getNomeTabella() + "WHERE id = ?");
-           tabella.setParam(stmt, 1, "id", id);
-           ResultSet r = stmt.executeQuery();  
-       if(r.next())
-       {
-           b.setiD(id);
-           b.setDataInizio(r.getString("data_inizio"));
-           b.setDataFine(r.getString("data_fine"));
-           
-       }
-          r.close();
-       return b;
+    public Bando cercaBandoPerId(int id) throws SQLException {
+        Bando b = new Bando();
+        
+        PreparedStatement stmt = tabella.prepareStatement("SELECT * FROM "
+                + tabella.getNomeTabella() + "WHERE id = ?");
+        tabella.setParam(stmt, 1, "id", id);
+        ResultSet r = stmt.executeQuery();
+        if (r.next()) {
+            b.setiD(id);
+            b.setDataInizio(r.getString("data_inizio"));
+            b.setDataFine(r.getString("data_fine"));
+            
+        }
+        r.close();
+        return b;
     }
     
     
     /**
      * ricerca bando per una data inizio
-     * @param dataInizio da ricercare nel database
-     * @return lista di bandi aventi come data inizio la data passata tra i parametri, oppure una lista vuota qualora non ci fossero bandi con quella data d'inizio
-     * @throws SQLException  se si verifica un errore di connessione
+     * 
+     * @param dataInizio
+     *            da ricercare nel database
+     * @return lista di bandi aventi come data inizio la data passata tra i
+     *         parametri, oppure una lista vuota qualora non ci fossero bandi
+     *         con quella data d'inizio
+     * @throws SQLException
+     *             se si verifica un errore di connessione
      */
-    public List<Bando> cercaPerDataInizio (String dataInizio) throws SQLException 
-    {
-        List<Bando> lb=new ArrayList<Bando>();
-        Bando b=new Bando();
-       
-        PreparedStatement stmt = tabella.prepareStatement(
-                "SELECT * FROM " + tabella.getNomeTabella() + "WHERE data_inizio = ?");
-            tabella.setParam(stmt, 1, "data_inizio", dataInizio);
-            ResultSet r = stmt.executeQuery();  
-        if(r.next())
-        {
+    public List<Bando> cercaPerDataInizio(String dataInizio)
+            throws SQLException {
+        List<Bando> lb = new ArrayList<Bando>();
+        Bando b = new Bando();
+        
+        PreparedStatement stmt = tabella.prepareStatement("SELECT * FROM "
+                + tabella.getNomeTabella() + "WHERE data_inizio = ?");
+        tabella.setParam(stmt, 1, "data_inizio", dataInizio);
+        ResultSet r = stmt.executeQuery();
+        if (r.next()) {
             b.setiD(r.getInt("id"));
             b.setDataInizio(r.getString("data_inizio"));
             b.setDataFine(r.getString("data_fine"));
             lb.add(b);
             
         }
-           r.close();
+        r.close();
         return lb;
-     }
+    }
     
     /**
      * ricerca bando per una specifica datafine
-     * @param dataFine è la data ricercata nel database per indivuduare i bandi
-     * @return lista di bandi aventi la datafine richiesta o una lista vuota se non esiste un bando avente come data di fine "datafine"
-     * @throws SQLException se si verifica un eccezione nella connessione al database
+     * 
+     * @param dataFine
+     *            è la data ricercata nel database per indivuduare i bandi
+     * @return lista di bandi aventi la datafine richiesta o una lista vuota se
+     *         non esiste un bando avente come data di fine "datafine"
+     * @throws SQLException
+     *             se si verifica un eccezione nella connessione al database
      */
-    public List<Bando> cercaPerDataFine (String dataFine) throws SQLException 
-    {
-        List<Bando> lb=new ArrayList<Bando>();
-        Bando b=new Bando();
-        Date id,fine=new Date();
-        PreparedStatement stmt = tabella.prepareStatement(
-                "SELECT * FROM " + tabella.getNomeTabella() + "WHERE data_fine = ?");
-            tabella.setParam(stmt, 1, "data_fine", dataFine);
-            ResultSet r = stmt.executeQuery();  
-        if(r.next())
-        {
+    public List<Bando> cercaPerDataFine(String dataFine) throws SQLException {
+        List<Bando> lb = new ArrayList<Bando>();
+        Bando b = new Bando();
+        Date id, fine = new Date();
+        PreparedStatement stmt = tabella.prepareStatement("SELECT * FROM "
+                + tabella.getNomeTabella() + "WHERE data_fine = ?");
+        tabella.setParam(stmt, 1, "data_fine", dataFine);
+        ResultSet r = stmt.executeQuery();
+        if (r.next()) {
             b.setiD(r.getInt("id"));
             b.setDataInizio(r.getString("data_inizio"));
             b.setDataFine(r.getString("data_fine"));
             lb.add(b);
             
         }
-           r.close();
+        r.close();
         return lb;
     }
-
-  
+    
+    /**
+     * dato che il bando è uno solo invece di fare i metodi di ricerca fammi un
+     * solo metodo che mi restituisce un bando con il record completo
+     * 
+     * @return
+     */
+    public Bando getBAndo() {
+        return null;
+    }
+    
+    
 }
