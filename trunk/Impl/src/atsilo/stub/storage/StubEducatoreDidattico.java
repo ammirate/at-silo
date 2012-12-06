c2f4de282be7c00d39cc517080a5aba255bf7fd1
marco.parisi90@gmail.com <marco.parisi90@gmail.com@02549b1f-f713-6533-8de7-45dd18cc345b>
2012-12-06 15:20:34 +0000

    [WT M] Creazione stub

---
+++ trunk/Impl/src/atsilo/stub/storage/StubEducatoreDidattico.java
@@ -0,0 +1,40 @@
+/*
+ *-----------------------------------------------------------------
+ * This file is licensed under GPL 3.0:
+ * http://www.gnu.org/licenses/gpl-3.0.html
+ *-----------------------------------------------------------------
+ * FILE: StubEducatoreDidattico.java
+ *-----------------------------------------------------------------
+ * PROGETTO: Atsilo
+ *-----------------------------------------------------------------
+ * OWNER
+ * Marko, 06/dic/2012
+ * REVISION
+ * <nome revisore>, <data revisione>
+ *-----------------------------------------------------------------
+ */
+
+package atsilo.stub.storage;
+
+/**
+ * Classe StubEducatoreDidattico
+ * <Descrizione classe>
+ * 
+ * @author Marko
+ * 
+ */
+public class StubEducatoreDidattico {
+    //Logger
+    /*
+     * private static final java.util.Logger LOG
+     *         = Logger.getLogger(StubEducatoreDidattico.class.getName());
+     */
+    
+    //Variabili di istanza
+    
+    
+    //Costruttori
+    
+    
+    //Metodi
+}
---
+++ trunk/Impl/src/atsilo/stub/storage/StubPsicopedagogo.java
@@ -0,0 +1,40 @@
+/*
+ *-----------------------------------------------------------------
+ * This file is licensed under GPL 3.0:
+ * http://www.gnu.org/licenses/gpl-3.0.html
+ *-----------------------------------------------------------------
+ * FILE: StubPsicopedagogo.java
+ *-----------------------------------------------------------------
+ * PROGETTO: Atsilo
+ *-----------------------------------------------------------------
+ * OWNER
+ * Marko, 06/dic/2012
+ * REVISION
+ * <nome revisore>, <data revisione>
+ *-----------------------------------------------------------------
+ */
+
+package atsilo.stub.storage;
+
+/**
+ * Classe StubPsicopedagogo
+ * <Descrizione classe>
+ * 
+ * @author Marko
+ * 
+ */
+public class StubPsicopedagogo {
+    //Logger
+    /*
+     * private static final java.util.Logger LOG
+     *         = Logger.getLogger(StubPsicopedagogo.class.getName());
+     */
+    
+    //Variabili di istanza
+    
+    
+    //Costruttori
+    
+    
+    //Metodi
+}
