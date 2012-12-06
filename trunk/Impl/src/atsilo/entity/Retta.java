cb448020b390922c6674f300f5fa8a8988dd3e71
yurilol@gmail.com <yurilol@gmail.com@02549b1f-f713-6533-8de7-45dd18cc345b>
2012-12-06 17:49:16 +0000

    [WR A] Aggiunti nuovi attributi a Retta.

--- trunk/Impl/src/atsilo/entity/Retta.java
+++ trunk/Impl/src/atsilo/entity/Retta.java
@@ -1,5 +1,7 @@
 package atsilo.entity;
+
 import java.util.List;
+import java.sql.Date;
 
 /*
  *-----------------------------------------------------------------
@@ -12,13 +14,24 @@ import java.util.List;
  *-----------------------------------------------------------------
  * OWNER
  * Antonio Cesarano, 14/11/2012 (non responsabile)
+ * REVISION
+ * Andrea Micco, 6/12/2012
  *-----------------------------------------------------------------
  */
 
 public class Retta {
     
-    private float importo;
+    
+   
+   
     private String id;
+    private String descrizioneTassa;
+    private String descrizioneRata;
+    private Date data;
+    private float importoDovuto;
+    private float importoPagato;
+    private String nome;
+    
     
     /**
      * La variabile extra è nulla c'è bisogno di una lettura della classe Extra
@@ -36,17 +49,29 @@ public class Retta {
     public Retta(){}
     
     /**
-     * @param importo il paramentro è fissato
+     * @param importoDovuto il paramentro è fissato
      * @param id il paramentro è fissato
      * @param extra è settato dopo la lettura 
      * @param genitore è settato dopo la lettura 
+     * @param importoPagato il parametro è fissato
+     * @param descrizioneTassa il parametro è fissato
+     * @param descrizioneRata il parametro è fissato
+     * @param data il parametro è fissato
+     * @param nome il parametro è fissato
      */
-    public Retta(float importo, String id, List<Extra> extra, Genitore genitore) {
+    public Retta(float importoDovuto, String id, List<Extra> extra, Genitore genitore,
+            float importoPagato, String descrizioneTassa, String descrizioneRata, 
+            Date data, String nome) {
         super();
-        this.importo = importo;
+        this.importoDovuto = importoDovuto;
         this.id = id;
         this.extra = extra;
         this.genitore = genitore;
+        this.importoPagato = importoPagato;
+        this.descrizioneTassa = descrizioneTassa;
+        this.descrizioneRata = descrizioneRata;
+        this.data = data;
+        this.nome = nome;
     }
 
     /**
@@ -66,21 +91,103 @@ public class Retta {
         }
     
     /**
-     * Metodo che prende il valore importo
-     * @param importo il parametro inserito
+     * Metodo che prende il valore importoDovuto
+     * @param importoDovuto il parametro inserito
      */
-    public void setImporto(float importo){
-        this.importo=importo;
+    public void setImportoDovuto(float importoDovuto){
+        this.importoDovuto=importoDovuto;
         }
     
     /**
-     * Metodo che prende il valore importo
-     * @return importo il parametro inserito
+     * Metodo che prende il valore importoDovuto
+     * @return importoDovuto il parametro inserito
      */
-    public float getImporto(){
-        return this.importo;
+    public float getImportoDovuto(){
+        return this.importoDovuto;
         }
 
+    /**
+     * Metodo che prende il valore importoPagato
+     * @param importoPagato il parametro inserito
+     */
+    public void setImportoPagato(float importoPagato){
+        this.importoPagato=importoPagato;
+        }
+    
+    /**
+     * Metodo che prende il valore importoPagato
+     * @return importoPagato il parametro inserito
+     */
+    public float getImportoPagato(){
+        return this.importoPagato;
+        }
+    
+    /**
+     * Metodo che prende il valore descrizioneTassa
+     * @param descrizioneTassa il parametro inserito
+     */
+    public void setDescrizioneTassa(String descrizioneTassa){
+        this.descrizioneTassa=descrizioneTassa;
+        }
+    
+    /**
+     * Metodo che prende il valore descrizioneTassa
+     * @return descrizioneTassa il parametro inserito
+     */
+    public String getDescrizioneTassa(){
+        return this.descrizioneTassa;
+        }
+    
+    /**
+     * Metodo che prende il valore descrizioneRata
+     * @param descrizioneRata il parametro inserito
+     */
+    public void setDescrizioneRata(String descrizioneRata){
+        this.descrizioneRata=descrizioneRata;
+        }
+    
+    /**
+     * Metodo che prende il valore descrizioneRata
+     * @return descrizioneRata il parametro inserito
+     */
+    public String getDescrizioneRata(){
+        return this.descrizioneRata;
+        }
+    
+    /**
+     * Metodo che prende il valore nome
+     * @param nome il parametro inserito
+     */
+    public void setNome(String nome){
+        this.nome=nome;
+        }
+    
+    /**
+     * Metodo che prende il valore nome
+     * @return nome il parametro inserito
+     */
+    public String getNome(){
+        return this.nome;
+        }
+    
+    
+    /**
+     * Metodo che prende il valore data
+     * @param data il parametro inserito
+     */
+    public void setData(Date data){
+        this.data=data;
+        }
+    
+    /**
+     * Metodo che prende il valore data
+     * @return data il parametro inserito
+     */
+    public Date getData(){
+        return this.data;
+        }
+    
+    
     /**
      * Metodo che setta il parametro letto dalla classe Extra
      * @param extra è settato dopo la lettura
