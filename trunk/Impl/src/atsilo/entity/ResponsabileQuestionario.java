2bf3f67110ca90e54829bf9fd3b3638fae68ba31
yurilol@gmail.com <yurilol@gmail.com@02549b1f-f713-6533-8de7-45dd18cc345b>
2012-12-06 14:08:44 +0000

    [WT A] Creato bean ResponsabileQuestionario

--- trunk/Impl/src/atsilo/entity/ResponsabileQuestionario.java
+++ trunk/Impl/src/atsilo/entity/ResponsabileQuestionario.java
@@ -1,9 +1,47 @@
 package atsilo.entity;
 
+import java.sql.Date;
+/*
+ *-----------------------------------------------------------------
+ * This file is licensed under GPL 3.0:
+ * http://www.gnu.org/licenses/gpl-3.0.html
+ *-----------------------------------------------------------------
+ * FILE: Utente.java
+ *-----------------------------------------------------------------
+ * PROGETTO: Atsilo
+ *-----------------------------------------------------------------
+ * OWNER
+ * Andrea Micco, 5/12/2012 (non responsabile)
+ *-----------------------------------------------------------------
+ */
+
 public class ResponsabileQuestionario extends Utente{
+    
+    /**
+     * Costruttore vuoto
+     */
     
     public ResponsabileQuestionario() {
     }
+    
+    /**
+     *  
+     */
+    public ResponsabileQuestionario(Date dataNascita, String nome, String cognome,
+            String codiceFiscale, String email, String comuneNascita,
+            String telefono, String cittadinanza, String indirizzoResidenza,
+            String numeroCivicoResidenza, String capResidenza, String comuneResidenza,
+            String provinciaResidenza, String indirizzoDomicilio,
+            String numeroCivicoDomicilio, String capDomicilio, String comuneDomicilio,
+            String provinciaDomicilio) {
+        super(dataNascita, nome, cognome,
+                codiceFiscale, email, comuneNascita,
+              telefono, cittadinanza, indirizzoResidenza,
+                numeroCivicoResidenza, capResidenza, comuneResidenza,
+                provinciaResidenza, indirizzoDomicilio,
+               numeroCivicoDomicilio, capDomicilio,comuneDomicilio,
+               provinciaDomicilio);
 
+    }
     
 }
