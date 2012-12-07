010bec2e1024e02c6800d569fac1a4304529e35f
marco.parisi90@gmail.com <marco.parisi90@gmail.com@02549b1f-f713-6533-8de7-45dd18cc345b>
2012-12-07 18:07:29 +0000

    [Wt M] creazione ControlloginTest

---
+++ trunk/Impl/src/atsilo/test/application/ControlLoginTest.java
@@ -0,0 +1,42 @@
+/*
+ *-----------------------------------------------------------------
+ * This file is licensed under GPL 3.0:
+ * http://www.gnu.org/licenses/gpl-3.0.html
+ *-----------------------------------------------------------------
+ * FILE: ControlLoginTest.java
+ *-----------------------------------------------------------------
+ * PROGETTO: Atsilo
+ *-----------------------------------------------------------------
+ * OWNER
+ * Marko, 07/dic/2012
+ * REVISION
+ * <nome revisore>, <data revisione>
+ *-----------------------------------------------------------------
+ */
+
+package atsilo.test.application;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Classe ControlLoginTest
+ * <Descrizione classe>
+ * 
+ * @author Marko
+ * 
+ */
+public class ControlLoginTest {
+    
+   
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
