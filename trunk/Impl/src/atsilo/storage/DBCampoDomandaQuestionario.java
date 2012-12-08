1ef4d7aaf0f09fe5a119935c4b0bb7c44bedb5ed
fabionapoli34@gmail.com <fabionapoli34@gmail.com@02549b1f-f713-6533-8de7-45dd18cc345b>
2012-12-08 15:04:41 +0000

    [WR] modifiche nei commenti

--- trunk/Impl/src/atsilo/storage/DBCampoDomandaQuestionario.java
+++ trunk/Impl/src/atsilo/storage/DBCampoDomandaQuestionario.java
@@ -21,7 +21,7 @@ import atsilo.storage.DBBeans.Assegnazione;
  * This file is licensed under GPL 3.0:
  * http://www.gnu.org/licenses/gpl-3.0.html
  *-----------------------------------------------------------------
- * FILE: DBDomandaQuestionario.java
+ * FILE: DBCampoDomandaQuestionario.java
  *-----------------------------------------------------------------
  * PROGETTO: Atsilo
  *-----------------------------------------------------------------
