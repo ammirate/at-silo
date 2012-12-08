ae94184b27f10c562491fb1f24763ba0c962578b
gianfranco.b.d <gianfranco.b.d@gmail.com>
2012-12-08 16:47:44 +0000

    [WT A] aggiunta test

---
+++ Impl/src/atsilo/application/notificheMail/Testnotifichemail.java
@@ -0,0 +1,55 @@
+/*
+ *-----------------------------------------------------------------
+ * This file is licensed under GPL 3.0:
+ * http://www.gnu.org/licenses/gpl-3.0.html
+ *-----------------------------------------------------------------
+ * FILE: Testnotifichemail.java
+ *-----------------------------------------------------------------
+ * PROGETTO: Atsilo
+ *-----------------------------------------------------------------
+ * OWNER
+ * Frank, 08/dic/2012
+ * REVISION
+ * <nome revisore>, <data revisione>
+ *-----------------------------------------------------------------
+ */
+
+package atsilo.application.notificheMail;
+
+import java.util.ArrayList;
+
+import javax.mail.MessagingException;
+
+import atsilo.entity.Utente;
+
+import sun.rmi.runtime.NewThreadAction;
+
+/**
+ * Classe Testnotifichemail <Descrizione classe>
+ * 
+ * @author Frank
+ * 
+ */
+public class Testnotifichemail {
+    public static void main(String[] args) throws MessagingException, Throwable {
+        ControlNotificaMail posta = ControlNotificaMail.getInstance();
+        ArrayList<Utente> destinatari = new ArrayList<Utente>();
+        String testo = "";
+        String oggetto = "";
+        Utente licenziato = new Utente();
+        
+        Utente utente = new Utente();
+        utente.setEmail("frankino173@gmail.com");
+        licenziato.setEmail("gianfranco.b.d@gmail.com");
+        licenziato.setNome("nome");
+        licenziato.setCognome("cognome");
+        destinatari.add(utente);
+        destinatari.add(licenziato);
+        NotificaMailLicenziamento messaggio = new NotificaMailLicenziamento(
+                destinatari, testo, oggetto, licenziato);
+        posta.inviaMail(messaggio);
+        
+        
+    }
+    
+}
