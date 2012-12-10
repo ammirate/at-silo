10a1e8d888ed8cbcc7e18f6d80000f73687902bc
marco.parisi90 <marco.parisi90@gmail.com>
2012-12-10 11:00:57 +0000

    [WR M] inserito metodo modificaPianoPasto

--- Impl/src/atsilo/application/ControlMensa.java
+++ Impl/src/atsilo/application/ControlMensa.java
@@ -1,6 +1,13 @@
 package atsilo.application;
 
+import java.sql.SQLException;
+
+import atsilo.entity.Bando;
 import atsilo.entity.Servizio;
+import atsilo.exception.DBConnectionException;
+import atsilo.storage.DBServizio;
+import atsilo.storage.Database;
+import atsilo.stub.storage.StubBando;
 
 //TODO scrivere la javadoc
 
@@ -16,25 +23,60 @@ import atsilo.entity.Servizio;
  * OWNER
  * Antonio Cesarano, 27/11/2012 (non responsabile)
  *-----------------------------------------------------------------
+ *
  */
 
+/**
+ * 
+ * @author Marko
+ */
 public class ControlMensa {
     
-    private static final ControlMensa cme;
+    private static ControlMensa control = new ControlMensa();
+    private DBServizio dbServizio;
     
     private ControlMensa(){
         
     }
     public static final ControlMensa getInstance(){
-        return null;
+        return control;
     }
     public boolean inserisciPianoPasto(Servizio s, String pianopasto){
         return false;
         
     }
 
-    public boolean modificaPianoPasto(Servizio s, String pianopasto){
+    public boolean modificaPianoPasto(Servizio s, String pianopasto)throws DBConnectionException {
         
+        Database db = new Database();
+        if (!db.apriConnessione()) {
+            throw new DBConnectionException("Connessione Fallita");
+        }
+        try {
+            try {
+                Servizio servizio = new Servizio(
+                      
+                        dbServizio.ricercaServizioPerBambino(s.getBambino()).getId(),
+                        dbServizio.ricercaServizioPerBambino(s.getBambino()).getOrarioInizio(),
+                        dbServizio.ricercaServizioPerBambino(s.getBambino()).getOrarioFine(),
+                        dbServizio.ricercaServizioPerBambino(s.getBambino()).getPianoPasto(),
+                        dbServizio.ricercaServizioPerBambino(s.getBambino()).getBambino(),
+                        dbServizio.ricercaServizioPerBambino(s.getBambino()).getOrarioUtente());
+                servizio.setPianoPasto(pianopasto);
+                          
+                dbServizio.replace(dbServizio.ricercaServizioPerBambino(s.getBambino()), servizio);
+                
+            } catch (SQLException e) {
+                throw new DBConnectionException("Connessione Fallita");
+            }
+            return true;
+        } finally {
+            db.chiudiConnessione();
+        }
+    }
+    {
+        
+       
     }
     public boolean eliminaPianoPasto(Servizio s){
         return false;
