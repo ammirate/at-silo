df32775f3fffe3bc116c0588f9ff7dca8176b830
gianfranco.b.d@gmail.com <gianfranco.b.d@gmail.com@02549b1f-f713-6533-8de7-45dd18cc345b>
2012-12-10 13:23:08 +0000

    [WT A] modificac ControlGestioneBandoTest

--- trunk/Impl/src/atsilo/test/application/ControlGestioneBandoTest.java
+++ trunk/Impl/src/atsilo/test/application/ControlGestioneBandoTest.java
@@ -25,7 +25,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-
 import atsilo.application.ControlGestioneBando;
 import atsilo.entity.Bando;
 import atsilo.entity.DomandaIscrizione;
@@ -33,19 +32,20 @@ import atsilo.entity.DomandaIscrizione;
 import atsilo.exception.BandoException;
 import atsilo.exception.DBConnectionException;
 import atsilo.storage.Database;
-import atsilo.stub.storage.StubBando;
-import atsilo.stub.storage.StubDomandaIscrizione;
+import atsilo.storage.DBBando;
+import atsilo.storage.DBDomandaIscrizione;
 
 
 public class ControlGestioneBandoTest {
-   private  ControlGestioneBando  control=ControlGestioneBando.getIstance();
-   private Database db = new Database();
+    private ControlGestioneBando control = ControlGestioneBando.getIstance();
+    private Database db = new Database();
+    
+    
     @Before
     /**
      * @throws Exception
      */
-    public void setUp() throws Exception 
-    {
+    public void setUp() throws Exception {
         
     }
     
@@ -53,62 +53,91 @@ public class ControlGestioneBandoTest {
     /**
      * @throws Exception
      */
-    public void tearDown() throws Exception 
-    {
+    public void tearDown() throws Exception {
     }
     
     /**
-     * creo una domanda in locale e  gli do punteggio 0 poi la inserisco nel database e tramite il metodo inserisci punteggio cambio il punteggio 
-     * e verifico che sia andato a buon fine
-     * Test method for {@link atsilo.application.ControlGestioneBando#insrisciPunteggio(atsilo.entity.DomandaIscrizione, int)}.
-     * @throws BandoException 
-     * @throws DBConnectionException 
-     * @throws SQLException 
+     * creo una domanda in locale e gli do punteggio 0 poi la inserisco nel
+     * database e tramite il metodo inserisci punteggio cambio il punteggio e
+     * verifico che sia andato a buon fine Test method for
+     * {@link atsilo.application.ControlGestioneBando#insrisciPunteggio(atsilo.entity.DomandaIscrizione, int)}
+     * .
+     * 
+     * @throws BandoException
+     * @throws DBConnectionException
+     * @throws SQLException
      */
     @Test
-    public final void testInsrisciPunteggio() throws DBConnectionException, BandoException, SQLException 
-    {
-        StubDomandaIscrizione dbDomande=new StubDomandaIscrizione(db);
-        DomandaIscrizione domanda=new DomandaIscrizione();
+    public final void testInsrisciPunteggio() throws DBConnectionException,
+            BandoException, SQLException {
+        DBDomandaIscrizione dbDomande = new DBDomandaIscrizione(db);
+        DomandaIscrizione domanda = new DomandaIscrizione();
         
-        Date dataPresentazione=new Date(0);
+        Date dataPresentazione = new Date(0);
         
-        domanda.setDataPresentazione(dataPresentazione);
-        domanda.setId(12);
-        dbDomande.inserisciDomanda(domanda);
+        domanda.se
+        dbDomande.inserisci(domanda);
         control.inserisciPunteggio(domanda, 54);
         domanda = dbDomande.ricercaDomandaDaId(domanda.getId());
         
-        assertEquals(54,domanda.getPunteggio());
+        assertEquals(54, domanda.getPunteggio());
         
     }
     
     
     /**
-     * Test method for {@link atsilo.application.ControlGestioneBando#inserisciIntervalloBando(java.lang.String, java.lang.String)}.
-     * @throws BandoException 
-     * @throws DBConnectionException 
+     * Test method for
+     * {@link atsilo.application.ControlGestioneBando#modificaintervalli(java.lang.String, java.lang.String,java.lang.String,java.lang.String,java.lang.String)}
+     * .
+     * 
+     * @throws BandoException
+     * @throws DBConnectionException
+     * @throws SQLException
      */
     @Test
-    public final void testInserisciIntervalloBando() throws DBConnectionException, BandoException 
-    {
-        StubBando dbBando=new StubBando(db);
-        Bando bando=new Bando();
-        bando.setDataFineBando("ciao");
-        bando.setDataFineRinuncia("bye");
-        dbBando.inserisci(bando);
-        System.out.println("l'id è questo"+bando.getiD());
-        control.inserisciIntervalloBando("ciao", "fine");
+    public final void testInserisciIntervalloBando()
+            throws DBConnectionException, BandoException, SQLException {
+        DBBando dbBando = new DBBando(db);
+        Bando bando = new Bando();
+        bando.setiD(1);
+        bando.setDataInizioBando("10");
+        bando.setDataFineBando("11");
+        bando.setDataInizioPresentazioneRinuncia("12");
+        bando.setDataFinePresentazioneRinuncia("13");
+        bando.setDataFineRinuncia("14");
+        control.modificaintervalli("15", "16", "17", "18", "19");
+        
+        assertEquals("15", dbBando.getBAndo().getDataInizioBando());
+        assertEquals("16", dbBando.getBAndo().getDataFineBando());
+        assertEquals("17", dbBando.getBAndo()
+                .getDataInizioPresentazioneRinuncia());
+        assertEquals("18", dbBando.getBAndo()
+                .getDataFinePresentazioneRinuncia());
+        assertEquals("19", dbBando.getBAndo().getDataFineRinuncia());
+        
         
     }
     
     /**
-     * Test method for {@link atsilo.application.ControlGestioneBando#modificaBando(atsilo.entity.Bando, java.lang.String, java.lang.String)}.
+     * Test method for
+     * {@link atsilo.application.ControlGestioneBando#modificaPostiDisponibili(int}
+     * .
+     *
+     */
+     @Test 
+     public final void testModificaPostiDisponibili() 
+             throws DBConnectionException, BandoException, SQLException 
+      {
+         DBBando dbBando = new DBBando(db);
+         Bando bando = new Bando();
+         bando.setiD(1);
+         bando.setPostiDisponibili(40);
+         control.modificaPostiDisponibili(30);
+         assertEquals(30, dbBando.getBAndo().getPostiDisponibili());
+         
+         ;
+              
+     }
      
-    @Test
-    public final void testModificaBando()
-    {
-    }
-    */
     
 }
