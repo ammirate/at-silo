c3db5e4e4e88451234c9ac6afd3032ff956d50d4
gianfranco.b.d <gianfranco.b.d@gmail.com>
2012-12-11 02:09:01 +0000

    [WT A] modificac ControlGestioneBandoTest

--- Impl/src/atsilo/test/application/ControlGestioneBandoTest.java
+++ Impl/src/atsilo/test/application/ControlGestioneBandoTest.java
@@ -24,9 +24,11 @@ import org.junit.Test;
 
 import atsilo.application.ControlGestioneBando;
 import atsilo.entity.Bando;
+import atsilo.entity.DomandaIscrizione;
 
 import atsilo.exception.BandoException;
 import atsilo.exception.DBConnectionException;
+import atsilo.storage.DBDomandaIscrizione;
 import atsilo.storage.Database;
 import atsilo.storage.DBBando;
 
@@ -37,10 +39,9 @@ public class ControlGestioneBandoTest {
     
     
     /**
-     * DB DOMANDA NON FIXATO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-     * creo una domanda in locale e gli do punteggio 0 poi la inserisco nel
-     * database e tramite il metodo inserisci punteggio cambio il punteggio e
-     * verifico che sia andato a buon fine Test method for
+     * creo una domanda in locale e gli do punteggio 0 poi la inserisco nel database e tramite il
+     * metodo inserisci punteggio cambio il punteggio e verifico che sia andato
+     * a buon fine Test method for
      * {@link atsilo.application.ControlGestioneBando#insrisciPunteggio(atsilo.entity.DomandaIscrizione, int)}
      * .
      * 
@@ -48,52 +49,43 @@ public class ControlGestioneBandoTest {
      * @throws DBConnectionException
      * @throws SQLException
      */
-    /*@Test
+    @Test
     public final void testInsrisciPunteggio() throws DBConnectionException,
-    BandoException, SQLException {
+            BandoException, SQLException {
         
         if (!db.apriConnessione()) {
             throw new DBConnectionException("Connessione Fallita");
         }
         try {
-            Date data = new Date(234);
             DBDomandaIscrizione dbDomande = new DBDomandaIscrizione(db);
-            
-            DomandaIscrizione domanda = new DomandaIscrizione(data,999,104,45,null,null,"stato","certificato","certificato","privacy",
-                    true,true,true,true,true,true,true,true,true,"punteggio",12,null,"convalida");
-            
-            dbDomande.inserisci(domanda);
+            DomandaIscrizione domanda = dbDomande.ricercaDomandaDaId(1);
             control.inserisciPunteggio(domanda, 54);
-            domanda = dbDomande.ricercaDomandaDaId(domanda.getId());
-            
             assertEquals(54, domanda.getPunteggio());
         } finally {
             db.chiudiConnessione();
         }
         
     }
-    */
+    
     
     /**
-     *  
+     * 
      * @throws DBConnectionException
      * @throws BandoException
      * @throws SQLException
      */
-    @Test public final void testInserisciIntervalloBando() throws
-    DBConnectionException, BandoException, SQLException 
-    {
-        if(!db.apriConnessione()) 
-        { throw new
-            DBConnectionException("Connessione Fallita"); 
+    @Test
+    public final void testInserisciIntervalloBando()
+            throws DBConnectionException, BandoException, SQLException {
+        if (!db.apriConnessione()) {
+            throw new DBConnectionException("Connessione Fallita");
         }
-        try 
-        {
-            DBBando dbBando =new DBBando(db);
-            Bando bando = new Bando(); 
+        try {
+            DBBando dbBando = new DBBando(db);
+            Bando bando = new Bando();
             System.out.println(dbBando.getBAndo().getiD());
             bando.setiD(1);
-            bando.setDataInizioBando("10"); 
+            bando.setDataInizioBando("10");
             bando.setDataFineBando("11");
             bando.setDataInizioPresentazioneRinuncia("12");
             bando.setDataFinePresentazioneRinuncia("13");
@@ -103,30 +95,44 @@ public class ControlGestioneBandoTest {
             
             assertEquals("15", dbBando.getBAndo().getDataInizioBando());
             assertEquals("16", dbBando.getBAndo().getDataFineBando());
-            assertEquals("17", dbBando.getBAndo().getDataInizioPresentazioneRinuncia()); 
-            assertEquals("18", dbBando.getBAndo() .getDataFinePresentazioneRinuncia());
+            assertEquals("17", dbBando.getBAndo()
+                    .getDataInizioPresentazioneRinuncia());
+            assertEquals("18", dbBando.getBAndo()
+                    .getDataFinePresentazioneRinuncia());
             assertEquals("19", dbBando.getBAndo().getDataFineRinuncia());
             assertEquals(30, dbBando.getBAndo().getPostiDisponibili());
-          }
-        finally 
-        {
-            db.chiudiConnessione(); 
-        } 
+        } finally {
+            db.chiudiConnessione();
+        }
     }
     
-    /** Test method for {@link
+    /**
+     * Test method for {@link
      * atsilo.application.ControlGestioneBando#modificaPostiDisponibili(int} .
      * 
-     /*
-     * @Test public final void testModificaPath() throws DBConnectionException,
-     * BandoException, SQLException { if (!db.apriConnessione()) { throw new
-     * DBConnectionException("Connessione Fallita"); } try { DBBando dbBando =
-     * new DBBando(db); Bando bando = new Bando(); bando.setiD(1);
-     * bando.setDataInizioBando("10"); bando.setDataFineBando("11");
-     * bando.setDataInizioPresentazioneRinuncia("12");
-     * bando.setDataFinePresentazioneRinuncia("13");
-     * bando.setDataFineRinuncia("14"); bando.setPostiDisponibili(40);
-     * bando.setPath("ciaoo"); control.modificaPath("bye"); assertEquals("bye",
-     * dbBando.getBAndo().getPath()); } finally { db.chiudiConnessione(); } }
      */
+    @Test
+    public final void testModificaPath() throws DBConnectionException,
+            BandoException, SQLException {
+        if (!db.apriConnessione()) {
+            throw new DBConnectionException("Connessione Fallita");
+        }
+        try {
+            DBBando dbBando = new DBBando(db);
+            Bando bando = new Bando();
+            bando.setiD(1);
+            bando.setDataInizioBando("10");
+            bando.setDataFineBando("11");
+            bando.setDataInizioPresentazioneRinuncia("12");
+            bando.setDataFinePresentazioneRinuncia("13");
+            bando.setDataFineRinuncia("14");
+            bando.setPostiDisponibili(40);
+            bando.setPath("ciaoo");
+            control.modificaPath("bye");
+            assertEquals("bye", dbBando.getBAndo().getPath());
+        } finally {
+            db.chiudiConnessione();
+        }
+    }
+    
 }
