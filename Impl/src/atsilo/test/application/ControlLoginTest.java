a1176c1b58154180c4890d1d547073dffd16b012
marco.parisi90 <marco.parisi90@gmail.com>
2012-12-12 14:06:45 +0000

    [WD M] modifica ControlLogin e il suo test PARISI E BOTTIGLIERI

--- Impl/src/atsilo/test/application/ControlLoginTest.java
+++ Impl/src/atsilo/test/application/ControlLoginTest.java
@@ -16,32 +16,74 @@
 
 package atsilo.test.application;
 
+import java.sql.SQLException;
+
 import javax.security.auth.login.LoginException;
 
 import atsilo.application.ControlLogin;
 import atsilo.entity.Account;
+import atsilo.entity.PersonaleAsilo;
 import atsilo.exception.DBConnectionException;
+import atsilo.storage.DBPersonaleAsilo;
+import atsilo.storage.DBPsicopedagogo;
 import atsilo.storage.Database;
 
 /**
- * Classe ControlLoginTest
- * <Descrizione classe>
+ * Classe ControlLoginTest <Descrizione classe>
  * 
  * @author fabio
+ * @author Marko
+ * @author Frank
  * 
  */
-public class ControlLoginTest 
-{
+public class ControlLoginTest {
     
     /**
      * @param args
-     * @throws DBConnectionException 
-     * @throws LoginException 
+     * @throws DBConnectionException
+     * @throws LoginException
      */
-    public static void main(String[] args) throws LoginException, DBConnectionException {
+    
+    
+    public static void main(String[] args) {
         // TODO Scheletro generato automaticamente
-     
         
-        ControlLogin control=ControlLogin.getInstance();
         
+        ControlLogin control = ControlLogin.getInstance();
+        
+        Database db = new Database();
+        if (!db.apriConnessione()) {
+            
+            try {
+                throw new LoginException("Connessione fallita");
+            } catch (LoginException e) {
+                System.out.println(" connessione fallita");
+            }
+        }
+        try {
+            DBPersonaleAsilo asilo=new DBPersonaleAsilo(db);
+            try {
+                
+                PersonaleAsilo personale=asilo.getPersonaleAsiloPerCF("SNTNTN53N69B309D");
+                
+                String username = "a.senatore";
+                String password = "antonellas";
+                String tipo = personale.getCategoriaAppartenenza();
+                System.out.println(tipo+" "+personale.getCodiceFiscale()+" "+personale.getNome());
+                
+                try {
+                    Account account = control.getValoreLogin(username, password, "DIRETTORE");
+                    System.out.println(account.getOwner().getCodiceFiscale());
+                } catch (LoginException e) {
+                    System.out.println("username password o tipologia errati");
+                }
+            } catch (SQLException e1) {
+                System.out.println("codice fiscale non trovato ");
+            }
+            
+            
+        } finally {
+            db.chiudiConnessione();
+        }
+    }
 }
