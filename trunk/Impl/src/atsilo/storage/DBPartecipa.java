0b9de195a037f7acd690a244852e15b2ca6e429e
fabionapoli34@gmail.com <fabionapoli34@gmail.com@02549b1f-f713-6533-8de7-45dd18cc345b>
2012-12-15 19:48:24 +0000

    [WT]creazione classe

---
+++ trunk/Impl/src/atsilo/storage/DBPartecipa.java
@@ -0,0 +1,30 @@
+/*
+ *-----------------------------------------------------------------
+ * This file is licensed under GPL 3.0:
+ * http://www.gnu.org/licenses/gpl-3.0.html
+ *-----------------------------------------------------------------
+ * FILE: DBPartecipa.java
+ *-----------------------------------------------------------------
+ * PROGETTO: Atsilo
+ *-----------------------------------------------------------------
+ * OWNER
+ * fabio, 15/dic/2012
+ * REVISION
+ * <nome revisore>, <data revisione>
+ *-----------------------------------------------------------------
+ */
+
+package atsilo.storage;
+
+/**
+ * Classe DBPartecipa
+ * <Classe che gestisce la tabella N a N della relazione tra
+ * Classe e Evento>
+ * 
+ * @author fabio
+ * 
+ */
+public class DBPartecipa {
+    
+    
+}
