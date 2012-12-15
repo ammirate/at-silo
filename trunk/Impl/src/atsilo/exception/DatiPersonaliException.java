e8377e8b6f3a4d9f9a26313af6ce6c69bc57bc6f
gianfranco.b.d@gmail.com <gianfranco.b.d@gmail.com@02549b1f-f713-6533-8de7-45dd18cc345b>
2012-12-15 11:37:28 +0000

    [WT A] creazione DatiPersonaliException.java

---
+++ trunk/Impl/src/atsilo/exception/DatiPersonaliException.java
@@ -0,0 +1,31 @@
+/*
+ *-----------------------------------------------------------------
+ * This file is licensed under GPL 3.0:
+ * http://www.gnu.org/licenses/gpl-3.0.html
+ *-----------------------------------------------------------------
+ * FILE: DatiPersonaliEception.java
+ *-----------------------------------------------------------------
+ * PROGETTO: Atsilo
+ *-----------------------------------------------------------------
+ * OWNER
+ * Frank, 15/dic/2012
+ * REVISION
+ * <nome revisore>, <data revisione>
+ *-----------------------------------------------------------------
+ */
+
+package atsilo.exception;
+
+/**
+ * Classe DatiPersonaliEception
+ * <Descrizione classe>
+ * 
+ * @author Frank
+ * 
+ */
+public class DatiPersonaliEception {
+
+   public DatiPersonaliEception(String stringa){
+        super(stringa);
+    }
+}
