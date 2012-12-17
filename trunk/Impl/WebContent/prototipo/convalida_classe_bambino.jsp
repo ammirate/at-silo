40ad13cc6b2202e243da2c2f3d6cab7b4ac26df8
lucariot@hotmail.it <lucariot@hotmail.it@02549b1f-f713-6533-8de7-45dd18cc345b>
2012-12-17 20:35:38 +0000

    [WT] Aggiunta interfaccia "convalida classe" e servlet per eseguire la convalida.

---
+++ trunk/Impl/WebContent/prototipo/convalida_classe_bambino.jsp
@@ -0,0 +1,105 @@
+<%@
+	include file="atsilo_files/header.jsp" %>
+<%@ page import="atsilo.application.*,atsilo.entity.*"%>
+
+<%
+	if ((request.getParameter("successo")) != null) {
+		if (request.getParameter("successo").equals("y")) {
+			out.print("<script type=text/javascript>alert('Modifica salvata con successo')</script>");
+		}
+		else {
+			out.print("<script type=text/javascript>alert('Modifica fallita')</script>");
+		}
+	}
+%>
+
+
+<table width="100%" cellspacing="0" cellpadding="0" border="0">
+	<tbody>
+		<tr>
+			<td class="breadcrumb " align="left"><p>
+					</a>
+				</p>
+				<p>&nbsp;</p></td>
+		</tr>
+	</tbody>
+</table>
+<%@
+include file="atsilo_files/sidebar_delegato.jsp"%>
+<img width="209" border="0" alt="" height="1"
+	src="atsilo_images/clearpixel.gif">
+</td>
+<td class="content" valign="top" bgcolor="#ffffff"><table
+		cellspacing="10" cellpadding="0" border="0" width="100%">
+		<tbody>
+			<tr>
+				<td><table border="0">
+						<tbody>
+							<tr>
+								<td class="tplTitolo">
+								
+<form action="http://localhost:8080/Atsilo/ServletConvalidaClasseBambino" method="post">
+<% 
+%>
+<table class="tabella_form">
+
+<%
+ControlClassi crt = ControlClassi.getIstance();
+int i=0;
+int n= crt.bambiniConClasseDaConvalidare().size();
+if(n == 0){
+	out.print("<h1> Non è presente nessuna classe da convalidare</h1>");
+}
+String input = "<input type='checkbox' name='classi' value='";
+String endInput = "' />";
+for(i=0;i<n;i++){
+	String id = ""+crt.bambiniConClasseDaConvalidare().get(i).getClasse();
+	out.append("<tr><td>")
+		.append(input)
+		.append(crt.bambiniConClasseDaConvalidare().get(i).getCodiceFiscale()+"_"+id)
+		.append(endInput)
+		.append("Bambino : <b>")
+		.append(crt.bambiniConClasseDaConvalidare().get(i).getNome()+" "+crt.bambiniConClasseDaConvalidare().get(i).getCognome()+" "+crt.getClasseById(Integer.parseInt(id)).getSezione())
+		.append("</b></td></tr>");
+}
+%>
+</table>
+<input type="submit" value="Convalida" id="conv" name="convalida" />
+
+
+</form>
+								</td>
+							</tr>
+						</tbody>
+					</table></td>
+			</tr>
+			<tr>
+				<td><table cellspacing="0" cellpadding="0" border="0">
+						<tbody>
+							<tr>
+								<td><table width="598" cellspacing="0" cellpadding="0"
+										border="0">
+									</table></td>
+							</tr>
+							<tr>
+								<td><img border="0" alt="" height="20" width="1"
+									src="atsilo_files/clearpixel.gif" /></td>
+							</tr>
+						</tbody>
+					</table></td>
+			</tr>
+
+		</tbody>
+	</table></td>
+<td class="fasciadxvariabile"></td>
+</tr>
+<%@
+include file="atsilo_files/footer.jsp"%>
+
+
+
+
+
+
+</body>
+</html>
---
+++ trunk/Impl/src/atsilo/application/servlet/ServletConvalidaClasseBambino.java
@@ -0,0 +1,84 @@
+package atsilo.application.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import atsilo.application.*;
+import atsilo.entity.Bambino;
+import atsilo.exception.BambinoException;
+import atsilo.exception.ClasseException;
+import atsilo.exception.DBConnectionException;
+import atsilo.storage.DBBambino;
+import atsilo.storage.Database;
+
+/**
+ * Servlet implementation class ServletControlBandoIM
+ */
+@WebServlet("/ServletConvalidaClasseBambino")
+public class ServletConvalidaClasseBambino extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private ControlClassi crt;
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletConvalidaClasseBambino() {
+        crt = crt.getIstance();
+    }
+    
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter pw = response.getWriter();
+        pw.println("");
+        pw.println("");
+        pw.println("");
+        pw.println("<h1>Control Login</h1><br>This servlet should not be accessed this way.");
+        pw.println("");
+    }
+    
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        if(request.getParameter("classi") != null){
+            Database db = new Database();
+            DBBambino dbbamb = new DBBambino(db);
+            db.apriConnessione();
+            String[] classiSelezionate;
+            classiSelezionate = request.getParameterValues("classi");
+            String res[];
+            for(int i=0;i<classiSelezionate.length;i++){
+                res = classiSelezionate[i].split("_");
+                String cf = res[0];
+                int id = Integer.parseInt(res[1]);
+               try {
+                Bambino bamb = dbbamb.ricercaBambinoPerCodFiscale(cf);
+                crt.confermaBambinoNellaClasse(id, bamb);
+            } catch (SQLException e) {
+                response.setStatus(response.SC_MOVED_TEMPORARILY);
+                response.setHeader("Location", "prototipo/errore.html"); 
+            } catch (BambinoException e) {
+                response.setStatus(response.SC_MOVED_TEMPORARILY);
+                response.setHeader("Location", "prototipo/errore.html"); 
+            } catch (DBConnectionException e) {
+                response.setStatus(response.SC_MOVED_TEMPORARILY);
+                response.setHeader("Location", "prototipo/errore.html"); 
+            }
+                
+            }
+        }
+        response.setStatus(response.SC_MOVED_TEMPORARILY);
+        response.setHeader("Location", "prototipo/convalida_classe_bambino.jsp?"+"successo=y"); 
+    }
+}
