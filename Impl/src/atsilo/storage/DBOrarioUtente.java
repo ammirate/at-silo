1cb4b01e1f4339f944811af7a32df9da8d533923
funforums <funforums@gmail.com>
2012-12-18 23:05:40 +0000

    refactor

--- Impl/src/atsilo/storage/DBOrarioUtente.java
+++ Impl/src/atsilo/storage/DBOrarioUtente.java
@@ -2,6 +2,9 @@ package atsilo.storage;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -20,14 +23,49 @@ import atsilo.entity.Servizio;
  * Angelo Scafuro, Fabio Napoli, 17/11/2012 (non responsabili)
  *-----------------------------------------------------------------
  *///TODO da implementare priorità bassa
-public class DBOrarioUtente extends DBBeans {
+public class DBOrarioUtente extends DBBeans<OrarioUtente> {
     
     public DBOrarioUtente(Database db){
         super("orario_utente",db);
     }
+    
+    private static final Map<String,String> MAPPINGS=creaMapping();
+    private static final List<String> CHIAVE=creaChiave(); 
 
     public OrarioUtente ricercaOrarioUtenteDaId(int ID) {
         return null;
+    }
+
+    /**
+     * metodo che asocia alla chiave(nome campo db)
+     *  il rispettivo valore(nome attributo classe)
+     * @return
+     */
+    private static Map<String,String> creaMapping()
+    {
+        Map<String,String> res= new HashMap<String,String>();
+        res.put("id","id");
+        res.put("nome","nome");
+        res.put("oraInizio","ora_inizio");
+        res.put("oraFine","ora_fine");
+        res.put("dataInizio","data_inizio");
+        res.put("dataFine","data_fine");
+        res.put("descrizione","descrizione");
+        res.put("nome", "nome");
+        res.put( "prezzo","prezzo"); 
+        
+        return Collections.unmodifiableMap(res);
+    }
+    
+    
+    /**
+     * Metodo che crea la chiave
+     * @return
+     */
+    private static List<String> creaChiave(){
+        List<String> res=  Arrays.asList("id");
+        
+        return Collections.unmodifiableList(res);
     }
 
     /**
@@ -36,7 +74,7 @@ public class DBOrarioUtente extends DBBeans {
     @Override
     protected Map getMappingFields() {
         // TODO Scheletro generato automaticamente
-        return null;
+        return MAPPINGS;
     }
 
     /**
@@ -45,16 +83,25 @@ public class DBOrarioUtente extends DBBeans {
     @Override
     protected List getKeyFields() {
         // TODO Scheletro generato automaticamente
-        return null;
+        return CHIAVE;
     }
 
     /**
      * @see atsilo.storage.DBBeans#creaBean(java.sql.ResultSet)
      */
     @Override
-    protected Object creaBean(ResultSet r) throws SQLException {
-        // TODO Scheletro generato automaticamente
-        return null;
+    protected OrarioUtente creaBean(ResultSet r) throws SQLException {
+        OrarioUtente b=new OrarioUtente();
+        b.setId(r.getInt("id"));
+        b.setOraInizio(r.getString("ora_inizio"));
+        b.setOraFine(r.getString("ora_fine"));
+        b.setPrezzo(r.getInt("prezzo"));
+        b.setDataInizio(r.getString("data_inizio"));
+        b.setDataFine(r.getString("data_fine"));
+        b.setNome(r.getString("nome"));
+        b.setDescrizione(r.getString("descrizione"));
+        
+        return b;
     }
 
 }
