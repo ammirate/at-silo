74547b402d750b02d91c7e35ef0976c948b83d34
ano2607 <cesarano2607@gmail.com>
2012-12-18 11:51:43 +0000

    [WR 3] revisione DBEvento e testato inserimento

--- Impl/src/atsilo/test/application/JUnitTestControlEvento.java
+++ Impl/src/atsilo/test/application/JUnitTestControlEvento.java
@@ -16,14 +16,25 @@
 
 package atsilo.test.application;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import atsilo.application.ControlEvento;
+import atsilo.entity.Classe;
+import atsilo.entity.EducatoreDidattico;
+import atsilo.entity.EventPlanner;
 import atsilo.entity.Evento;
+import atsilo.exception.EventoException;
+import atsilo.storage.Database;
 
 /**
  * Classe JUnitTestControlEvento
@@ -39,6 +50,23 @@ public class JUnitTestControlEvento {
      */
     @Before
     public void setUp() throws Exception {
+        Calendar data = Calendar.getInstance();
+        data.set(2012, 11, 1);
+        Date dataEvento= new Date(data.getTimeInMillis());
+        
+        EventPlanner planner = new EducatoreDidattico();
+        
+        evento = new Evento("riunione dei rappresentanti di classe", "riunione",
+                "antoio@gmail.com, ", dataEvento, "riunione", planner, "/home/user/Scrivania/Riunione.pdf");
+        /*
+        Classe c1 = new Classe(2,"2A",null);
+        Classe c2 = new Classe(3,"2B",null);
+        List<Classe> classi = new ArrayList<Classe>();
+        classi.add(c1);
+        classi.add(c2);
+        
+        evento.setClassi(classi);
+        */
         
     }
     
@@ -48,20 +76,26 @@ public class JUnitTestControlEvento {
      */
     @After
     public void tearDown() throws Exception {
+        control.eliminaEvento(evento);
+        //evento = null;
     }
     
     /**
      * Test method for {@link atsilo.application.ControlEvento#inserisciEvento(java.lang.String, java.lang.String, java.util.List, java.sql.Date, java.lang.String, java.sql.Date, java.util.List, atsilo.entity.EventPlanner, java.lang.String)}.
+     * @throws EventoException 
      */
     @Test
-    public void testInserisciEvento() {
-        fail("Not yet implemented");
+    public void testInserisciEvento() throws EventoException {
+        control.inserisciEvento(evento); 
+        int id = evento.getId();
+        assertTrue(id!=0);
+
     }
     
     /**
      * Test method for {@link atsilo.application.ControlEvento#modificaEvento(atsilo.entity.Evento, java.lang.String, java.lang.String, java.util.List, java.sql.Date, java.lang.String, java.sql.Date, java.util.List, atsilo.entity.EventPlanner, java.lang.String)}.
      */
-    @Test
+   // @Test
     public void testModificaEvento() {
         fail("Not yet implemented");
     }
@@ -69,7 +103,7 @@ public class JUnitTestControlEvento {
     /**
      * Test method for {@link atsilo.application.ControlEvento#eliminaEvento(atsilo.entity.Evento)}.
      */
-    @Test
+    //@Test
     public void testEliminaEvento() {
         fail("Not yet implemented");
     }
@@ -77,7 +111,7 @@ public class JUnitTestControlEvento {
     /**
      * Test method for {@link atsilo.application.ControlEvento#getEventiPerOrganizzatore(atsilo.entity.EventPlanner)}.
      */
-    @Test
+    //@Test
     public void testGetEventiPerOrganizzatore() {
         fail("Not yet implemented");
     }
@@ -85,7 +119,7 @@ public class JUnitTestControlEvento {
     /**
      * Test method for {@link atsilo.application.ControlEvento#getEventiInData(java.sql.Date)}.
      */
-    @Test
+    //@Test
     public void testGetEventiInData() {
         fail("Not yet implemented");
     }
@@ -93,11 +127,15 @@ public class JUnitTestControlEvento {
     /**
      * Test method for {@link atsilo.application.ControlEvento#getEventiPerNome(java.lang.String)}.
      */
-    @Test
+    //@Test
     public void testGetEventiPerNome() {
         fail("Not yet implemented");
     }
     
+    
+    
+    
     Evento evento;
     ControlEvento control = ControlEvento.getIstance();
+    Database db = new Database();
 }
